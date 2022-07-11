15d4e37ff596634d9dfa433e374e7e6179654a44
PezzaD84 <89595349+PezzaD84@users.noreply.github.com>
2022-07-11 10:08:52 +0100

    Update Migration Script V3.sh

--- Migration Script V3.sh
+++ Migration Script V3.sh
@@ -7,16 +7,32 @@
 #########################################################################################
 
 #########################################################################################
-# Variables
+# Custom Variables
 #########################################################################################
 
 ORG="COMPANY NAME"
 
+enrollmentURL="https://YOURJSS.com/enrol"
+
+EnrollmentUser="ENROLLMENT USERNAME"
+
+EnrollmentPW="ENROLLMENT PASSWORD"
+
+ITEmail="itemail@company.com"
+
+# Set to True if you are using Jamf Connect
+JamfConnect="False"
+
+# Set to True if you want to remove local admin rights from all your users
+RemoveAdmin="False"
+
+#########################################################################################
+# Variables
+#########################################################################################
+
 DEP_NOTIFY_LOG="/var/tmp/depnotify.log"
 
 DEP_NOTIFY_APP="/Applications/Utilities/DEPNotify.app"
-
-enrollmentURL="ENROLLMENT URL OF NEW JAMF"
 
 JCApp="/Applications/Jamf Connect.app/Contents/Info.plist"
 
@@ -40,7 +56,7 @@ else
 fi
 
 #########################################################################################
-# Check Previous build and clean up
+# Check Previous DEP Logs and clean up
 #########################################################################################
 
 # Removing config files in /var/tmp
@@ -64,8 +80,8 @@ echo "Command: WindowStyle: Activate" >> $DEP_NOTIFY_LOG
 echo "Command: WindowTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
 echo "Command: Image: /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/Sync.icns" >> $DEP_NOTIFY_LOG
 echo "Command: MainTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
-echo "Command: MainText: This mac will now be migrated to the new $ORG MDM server. \n \n Please do not log out or turn off this device during this process. \n \n The process will now begin." >> $DEP_NOTIFY_LOG
-echo "Status: Device Migration in progress....." >> $DEP_NOTIFY_LOG
+echo "Command: MainText: This mac will now be migrated to the new $ORG MDM server. \n \n Please do not log out or turn off this device during this process. \n \n The migration will begin shortly" >> $DEP_NOTIFY_LOG
+echo "Status: Preparing migration....." >> $DEP_NOTIFY_LOG
 
 # Start DEPNotify app
 launchctl asuser $CURRENT_USER_ID open -a "$DEP_NOTIFY_APP" --args -path "$DEP_NOTIFY_LOG"
@@ -76,9 +92,14 @@ Sleep 10
 # Remove MDM Profile
 #########################################################################################
 
+echo "Command: WindowStyle: Activate" >> $DEP_NOTIFY_LOG
+echo "Command: WindowTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
+echo "Command: Image: /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/Sync.icns" >> $DEP_NOTIFY_LOG
+echo "Command: MainTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
+echo "Command: MainText: The migration process has now started! \n \n Sit tight while we do all the techy stuff." >> $DEP_NOTIFY_LOG
 echo "Status: Removing old MDM Profile....." >> $DEP_NOTIFY_LOG
 
-jamf removeMdmProfile
+##### jamf removeMdmProfile
 
 sleep 10
 
@@ -88,7 +109,7 @@ sleep 10
 
 echo "Status: Removing old Management Framework....." >> $DEP_NOTIFY_LOG
 
-jamf removeFramework
+##### jamf removeFramework
 
 sleep 10
 
@@ -100,7 +121,7 @@ echo "Command: WindowStyle: Activate" >> $DEP_NOTIFY_LOG
 echo "Command: WindowTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
 echo "Command: Image: /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/Sync.icns" >> $DEP_NOTIFY_LOG
 echo "Command: MainTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
-echo "Command: MainText: **PLEASE READ THE FOLLOWING TO ENROLL YOUR DEVICE** \n You will now be taken to the new $ORG Enrollment page. Please make a note of the details below to enrol your device. \n \n Username: ENROLLMENT ACCOUNT \n Password: ENROLLMENT PASSWORD \n \n Once logged in please leave all the details as default and click on Enroll and then follow the onscreen prompts." >> $DEP_NOTIFY_LOG
+echo "Command: MainText: **PLEASE READ THE FOLLOWING TO ENROLL YOUR DEVICE** \n You will now be taken to the new $ORG Enrollment page. Please make a note of the details below to enrol your device. \n \n Username: $EnrollmentUser \n Password: $EnrollmentPW \n \n Once logged in please leave all the details as default and click on Enroll and then follow the onscreen prompts." >> $DEP_NOTIFY_LOG
 echo "Status: Device Migration in progress....." >> $DEP_NOTIFY_LOG
 echo "Command: ContinueButton: Enroll" >> $DEP_NOTIFY_LOG
 
@@ -172,7 +193,7 @@ echo "Command: WindowStyle: Activate" > $DEP_NOTIFY_LOG
 echo "Command: WindowTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
 echo "Command: Image: /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/Sync.icns" >> $DEP_NOTIFY_LOG
 echo "Command: MainTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
-echo "Command: MainText: Final Migration checks in progress. \n \n Your device is nearly ready. \n \n Please sit tight while we do some final checks to get you up and running." >> $DEP_NOTIFY_LOG
+echo "Command: MainText: Final Migration checks in progress. \n \n Your device is nearly ready. \n \n Hang on while we do some final checks to get you up and running." >> $DEP_NOTIFY_LOG
 echo "Status: Final Migration checks in progress....." >> $DEP_NOTIFY_LOG
 
 # Start DEPNotify app
@@ -180,40 +201,50 @@ launchctl asuser $CURRENT_USER_ID open -a "$DEP_NOTIFY_APP" --args -path "$DEP_N
 
 sleep 10
 
-#########################################################################################
-# Check for Jamf connect install
-#########################################################################################
-
-while [ ! -f "$JCApp" ]; do
-	echo "Jamf connect not installed. Waiting for install to complete...."
-	JCApp="/Applications/Jamf Connect.app/Contents/Info.plist"
-	sleep 5
-done
+if [[ $JamfConnect == "True" ]]; then
+	#########################################################################################
+	# Check for Jamf connect install
+	#########################################################################################
+	
+	while [ ! -f "$JCApp" ]; do
+		echo "Jamf connect not installed. Waiting for install to complete...."
+		JCApp="/Applications/Jamf Connect.app/Contents/Info.plist"
+		sleep 5
+	done
 	echo "Jamf Connect Installed. Continuing migration...."
-
-#########################################################################################
-# Reset Login Sceen to Jamf Connect
-#########################################################################################
-
-$authchng -reset -JamfConnect -preAuth JamfConnectLogin:privileged
-
-#########################################################################################
-# Check If logged in user is admin and remove admin permissions 
-#########################################################################################
-
-if [[ $admins == "" ]]; then
-	echo "$user is not local admin, continuing migration....." 
+	
+	#########################################################################################
+	# Reset Login Sceen to Jamf Connect
+	#########################################################################################
+	
+	$authchng -reset -JamfConnect -preAuth JamfConnectLogin:privileged
 else
-	echo "$user is a local admin, removing permissions....."
+	echo "Jamf Connect not being used"
+	Sleep 5
+fi
+
+if [[ $RemoveAdmin == "True" ]]; then
+	#########################################################################################
+	# Check If logged in user is admin and remove admin permissions 
+	#########################################################################################
+	
+	if [[ $admins == "" ]]; then
+	echo "$user is not local admin, continuing migration....." 
+	else
+		echo "$user is a local admin, removing permissions....."
 	dseditgroup -o edit -d $user -t user admin
 	sleep 5
 	user=$(ls -l /dev/console | awk '{ print $3 }')
 	admins=$(dscl . read /Groups/admin GroupMembership | grep $user)
-	if [[ $admins == "" ]]; then
-		echo "$user has been removed from the local admins group. continuing migration....."
-	else
-		echo "$user is still local admin. Continuing migration to avoid failure loop."
+		if [[ $admins == "" ]]; then
+			echo "$user has been removed from the local admins group. continuing migration....."
+		else
+			echo "$user is still local admin. Continuing migration to avoid failure loop."
+		fi
 	fi
+else
+	echo "Users will keep local admin rights"
+	Sleep 5
 fi
 
 #########################################################################################
@@ -224,6 +255,6 @@ echo "Command: WindowStyle: Activate" >> $DEP_NOTIFY_LOG
 echo "Command: WindowTitle: $ORG Migration" >> $DEP_NOTIFY_LOG
 echo "Command: Image: /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/Sync.icns" >> $DEP_NOTIFY_LOG
 echo "Command: MainTitle: Migration Complete!!" >> $DEP_NOTIFY_LOG
-echo "Command: MainText: This mac has been successfully migrated to the new $ORG MDM server. \n \n Please save and close any open work and then click 'Finish'. \n \n You will be logged out and taken to the new next15 login screen. \n \n Please log in with your next15 email address and follow the on screen prompts." >> $DEP_NOTIFY_LOG
+echo "Command: MainText: This mac has been successfully migrated to the new $ORG MDM server. \n \n Please save and close any open work and then click 'Finish' to log out and complete the Enrollment. \n \n If you have any issues please contact IT $ITEmail" >> $DEP_NOTIFY_LOG
 echo "Status: MIGRATION COMPLETE!" >> $DEP_NOTIFY_LOG
 echo "Command: ContinueButtonLogout: Finish" >> $DEP_NOTIFY_LOG
